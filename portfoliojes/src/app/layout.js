021bfe2de882a7737c72274250fce3321aa82a3d
jespasa <jose.espasa.saval@gmail.com>
2025-03-18 10:32:47 +0100

    rollback to react 18 version and update metadata

--- portfoliojes/src/app/layout.js
+++ portfoliojes/src/app/layout.js
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Viewport from "next";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -12,14 +13,34 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export const metadata = {
   title: "JES - Portfolio",
+  applicationName: "JES - Portfolio",
   description: "Personal porfolio",
+  authors: [{ name: "Jose Espasa Saval" }],
+  creator: "Jose Espasa Saval",
+  language: "en",
+  copyright: "Copyright © 2025",
+  contentType: "text/html; charset=utf-8",
+  handheldFriendly: "True",
+  revised: "Thursday, March 18th, 2025, 9:15 am",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="copyright" content={metadata.copyright} />
+        <meta name="HandheldFriendly" content={metadata.handheldFriendly} />
+        <meta name="revised" content={metadata.revised} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
